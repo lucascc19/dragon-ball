ac2ecffc51fc53547e23bd890d7ef23b3967a2a3
Lucas Oliveira <lucasrodriguesmano@gmail.com>
2022-09-15 14:56:28 -0300

    refatoracao v1

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,42 @@
+import Personagem from "./personagem";
+import prompt from "prompt-sync";
 
+let person: Personagem = new Personagem("Son Goku", 60, 100, 98, 97);
 
+let teclado = prompt();
+let option: number = 0;
+
+while(option != 6){
+    console.log('======= Personagem ========');
+    console.log('|1. Treinar ataque');
+    console.log('|2. Treinar defesa');
+    console.log('|3. Descansar');
+    console.log('|4. Entrar em batalha');
+    console.log('|5. Imprimir atributos');
+    console.log('|6. Sair');
+    console.log('===========================');
+
+    option = +teclado('Escolha uma ação: ');
+
+    switch (option){
+        case 1:
+            person.treinarAtaque();
+            person.status()
+            break;
+        case 2:
+            person.treinarDefesa();
+            person.status();
+        case 3:
+            person.descansar();
+            person.status();
+            break;
+        case 4:
+            person.batalhar();
+            person.status();
+            break;
+        case 5:
+            person.status();
+        default:
+            break;
+    }
+}
--- src/personagem.ts
+++ src/personagem.ts
@@ -1,98 +1,54 @@
-import prompt from "prompt-sync";
-
 //nome, energia, vida, ataque, defesa
-class Personagem {
-    nome: string = ""; 
-    energia: number = 0; 
-    vida: number = 0; 
-    ataque: number = 0; 
-    defesa: number = 0;
-}
+export default class Personagem {
+    constructor(
+        public nome: string,
+        public energia: number,
+        public vida: number,
+        public ataque: number,
+        public defesa: number    
+    ) {}
 
-let person: Personagem = new Personagem();
+    status(): void {
+        console.log("--- Guerreiro ---");
+            console.log("Nome: ", this.nome);
+            console.log("Energia: ", this.energia.toFixed(1));
+            console.log("Ataque: ", this.ataque.toFixed(1));
+            console.log("Defesa: ", this.defesa.toFixed(1));
+    }
+    
+    treinarAtaque(): void {
+        this.ataque += Math.random() * 7;
+        this.energia -= Math.random() * 10;
+        this.isDead();
+        if(this.defesa > 100){
+            this.defesa = 0;
+        } 
+    }
 
-person.nome = "Son Goku";
-person.ataque = 98;
-person.defesa = 97;
-person.energia = 95;
-person.vida = 100;
+    treinarDefesa(): void {
+        this.defesa += Math.random() * 5;
+        this.energia -= Math.random() * 10;
+        this.isDead();
+        if(this.defesa > 100){
+            this.defesa = 0;
+        }
+    }
 
-let teclado = prompt();
-let option: number = 0;
-while(option != 6){
-    console.log('======= Personagem ========');
-    console.log('|1. Treinar ataque');
-    console.log('|2. Treinar defesa');
-    console.log('|3. Descansar');
-    console.log('|4. Entrar em batalha');
-    console.log('|5. Imprimir atributos');
-    console.log('|6. Sair');
-    console.log('===========================');
+    descansar(): void {
+        this.energia += Math.random() * 10;
+        if(this.energia > 100){
+            this.energia = 100;
+        }
+    }
 
-    option = +teclado('Escolha uma ação: ');
+    batalhar(): void {
+        this.energia -= Math.random() * 100;
+        this.isDead();
+    }
 
-    switch (option){
-        case 1:
-            person.ataque += Math.random() * 7;
-            person.energia -= Math.random() * 10;
-            if(person.energia < 0){
-                console.log('Ops! Você morreu!');
-            }else{
-                if(person.defesa > 100){
-                    person.defesa = 100;
-                }
-            }
-            console.log("--- Guerreiro ---");
-            console.log("Nome: ", person.nome);
-            console.log("Energia: ", person.energia.toFixed(1));
-            console.log("Ataque: ", person.ataque.toFixed(1));
-            console.log("Defesa: ", person.defesa.toFixed(1));
-            break;
-        case 2:
-            person.defesa += Math.random() * 5;
-            person.energia -= Math.random() * 10;
-            if(person.energia < 0){
-                console.log('Ops! Você morreu!');
-            }else{
-                if(person.defesa > 100){
-                    person.defesa = 100;
-                }
-            }
-            console.log("--- Guerreiro ---");
-            console.log("Nome: ", person.nome);
-            console.log("Energia: ", person.energia.toFixed(1));
-            console.log("Ataque: ", person.ataque.toFixed(1));
-            console.log("Defesa: ", person.defesa.toFixed(1));
-        case 3:
-            person.energia += Math.random() * 10;
-            if(person.energia > 100){
-                person.energia = 100;
-            }
-            console.log("--- Guerreiro ---");
-            console.log("Nome: ", person.nome);
-            console.log("Energia: ", person.energia.toFixed(1));
-            console.log("Ataque: ", person.ataque.toFixed(1));
-            console.log("Defesa: ", person.defesa.toFixed(1));
-            break;
-        case 4:
-            person.energia -= Math.random() * 10;
-            if(person.energia<0){
-                console.log('Ops! Você morreu!');
-            }
-            console.log("--- Guerreiro ---");
-            console.log("Nome: ", person.nome);
-            console.log("Energia: ", person.energia.toFixed(1));
-            console.log("Ataque: ", person.ataque.toFixed(1));
-            console.log("Defesa: ", person.defesa.toFixed(1));
-            break;
-        case 5:
-            console.log("--- Guerreiro ---");
-            console.log("Nome: ", person.nome);
-            console.log("Energia: ", person.energia.toFixed(1));
-            console.log("Ataque: ", person.ataque.toFixed(1));
-            console.log("Defesa: ", person.defesa.toFixed(1));
-            break;
-        default:
-            break;
+    isDead(): void {
+        if(this.energia < 0){
+            console.log('Ops! Você morreu!');
+        }
     }
 }
